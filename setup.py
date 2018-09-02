e7ef2e0f0871c3043b57fcb03f489d08adba854f
willbelucky <jaekyoungkim@kaist.ac.kr>
2018-09-03 01:10:37 +0900

    2018.9.3 18-add_data_columns BUG FIX: We must use reset_index after selecting rows from DataFrame.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = 'ksif',
-    version          = '2018.8.31',
+    version          = '2018.09.03',
     description      = 'Quantitative investment tools for KSIF',
     long_description = long_description,
     author           = 'KSIF Tech',
