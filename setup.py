1e96d453722a1bbb164f7b5211845b1ea33f0cc7
willbelucky <jaekyoungkim@kaist.ac.kr>
2018-09-07 02:21:08 +0900

    2018.09.07 1. 투자자별 순매수량 데이터 1일 데이터를 20일 데이터로 교체 2. 금융필터를 FN_GUIDE_SECTOR != '금융' 으로 교체 3. SHORT_SALES_BALANCE_MOM 추가 4. SHARE_LENDING_VOLUME_RATIO, SHARE_LENDING_BALANCE_RATIO, SHARE_LENDING_BALANCE_MOM 추가 5. pf.show_plot(weight=True) 시 그래프가 깨지는 에러 수정. RET_1 == nan 이던 데이터 모두 거래정지로 처리하고, show_plot 메소드에 dropna(subset=RET_1) 추가.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = 'ksif',
-    version          = '2018.09.03',
+    version          = '2018.09.07',
     description      = 'Quantitative investment tools for KSIF',
     long_description = long_description,
     author           = 'KSIF Tech',
