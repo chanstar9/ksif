13cdf04849d45212490286a4b92e156dfab06523
willbelucky <jaekyoungkim@kaist.ac.kr>
2018-09-16 20:02:09 +0900

    30-quantile_distribution_ratio_add_bar_chart 1. Add an option for bar chart. 2. Add a test case for quantile_distribution_ratio

---
+++ ksif/tests/frame/test_quantile_distribution_ratio.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Jaekyoung Kim
+:Date: 2018-09-16
+"""
+from unittest import TestCase
+
+from ksif import Portfolio
+from ksif.core.columns import *
+
+
+class TestQuantileDistributionRatio(TestCase):
+
+    def test_quantile_distribution_ratio(self):
+        pf = Portfolio()
+
+        # When chunk_num is less than or equal to 1, raise an assertion error.
+        with self.assertRaises(AssertionError):
+            pf.quantile_distribution_ratio(factor=MKTCAP, chunk_num=1)
+        with self.assertRaises(AssertionError):
+            pf.quantile_distribution_ratio(factor=MKTCAP, chunk_num=-1)
+
+        # All true parameters with a title.
+        # The length of the result data frame is equal to original unique DATE minus one.
+        date_num = len(pf[DATE].unique())
+        result_data = pf.quantile_distribution_ratio(factor=MKTCAP, cumulative=True, weighted=True, only_positive=True,
+                                                     show_plot=True, show_bar_chart=True, title='title')
+        self.assertEqual(date_num - 1, len(result_data))
+
+        # All true parameters with a title.
+        # The length of the result data frame is equal to original unique DATE minus one.
+        date_num = len(pf[DATE].unique())
+        result_data = pf.quantile_distribution_ratio(factor=MKTCAP, cumulative=False, weighted=False,
+                                                     only_positive=False, show_plot=False, show_bar_chart=False)
+        self.assertEqual(date_num - 1, len(result_data))
