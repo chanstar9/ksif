df4b889628f8d8ab49482059db06ffdc7dd61c32
willbelucky <jaekyoungkim@kaist.ac.kr>
2018-11-07 23:19:49 +0900

    2018.11.07

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = 'ksif',
-    version          = '2018.09.19',
+    version          = '2018.11.07',
     description      = 'Quantitative investment tools for KSIF',
     long_description = long_description,
     author           = 'KSIF Tech',
