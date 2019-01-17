b753a9f32ec2193659b1aeec0deb2f353ca19eec
willbe <rlakim5521@naver.com>
2019-01-17 17:59:35 +0900

    version control

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = 'ksif',
-    version          = '2018.11.08',
+    version          = '2019.1.17',
     description      = 'Quantitative investment tools for KSIF',
     long_description = long_description,
     author           = 'KSIF Tech',
