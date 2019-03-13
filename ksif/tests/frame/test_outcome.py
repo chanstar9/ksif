ae4c74bd7fb24aa03e644d2d9d1d79230a1abcca
Jaekyoung Kim <jaekyoungkim@kaist.ac.kr>
2019-03-13 16:46:57 +0900

    Possible to calculate long-short portfolio outcome

--- ksif/tests/frame/test_outcome.py
+++ ksif/tests/frame/test_outcome.py
@@ -6,15 +6,41 @@
 from unittest import TestCase
 
 import pandas as pd
+import numpy as np
+from datetime import datetime
 
 from ksif import *
 
 
 class TestOutcome(TestCase):
 
-    def test_outcome(self):
+    def test_show_plot(self):
         pf = Portfolio()
 
-        outcome = pf.outcome()
+        outcome = pf.outcome(show_plot=True)
+        for value in outcome.values():
+            self.assertFalse(pd.isna(value))
+
+    def test_long_only_weighted(self):
+        pf = Portfolio()
+
+        outcome = pf.outcome(weighted=MKTCAP)
+        for value in outcome.values():
+            self.assertFalse(pd.isna(value))
+
+    def test_long_short_weighted(self):
+        pf = Portfolio()
+        pf['weight'] = np.where(pf[PER] > 0, 1, -1)
+
+        outcome = pf.outcome(weighted='weight')
+        for value in outcome.values():
+            self.assertFalse(pd.isna(value))
+
+    def test_no_short_period(self):
+        pf = Portfolio()
+        pf['weight'] = np.where(pf[PER] > 0, 1, -1)
+        pf.loc[pf[DATE] == datetime(2019, 1, 31), 'weight'] = 1
+
+        outcome = pf.outcome(weighted='weight')
         for value in outcome.values():
             self.assertFalse(pd.isna(value))
